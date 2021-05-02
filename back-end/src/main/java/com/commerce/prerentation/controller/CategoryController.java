8bea78f677949a8abbe6a87fcb21872e7eeeedb3
Truong <truong02bp@github.com>
2021-05-02 17:55:59 +0700

    create

--- back-end/src/main/java/com/commerce/prerentation/controller/CategoryController.java
+++ back-end/src/main/java/com/commerce/prerentation/controller/CategoryController.java
@@ -1,11 +1,23 @@
 package com.commerce.prerentation.controller;
 
+import com.commerce.data.entities.Category;
+import com.commerce.service.CategoryService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@AllArgsConstructor
 public class CategoryController {
 
+    private final CategoryService categoryService;
 
+    @PostMapping("/category")
+    public ResponseEntity<Object> create(Category category){
+        Category res = categoryService.create(category);
+        return ResponseEntity.ok(res);
+    }
 }
